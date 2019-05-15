ef913488c9e8b8c561551193fa914d87740812d0
Gustavo Bellini Bigardi <gustavo.bigardi@dextra-sw.com>
2019-05-15 16:10:57 -0300

    adding 3rd part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import TaskButtons from "./components/task-buttons";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tasks: [
-        { id: 1, description: 'Ir ao supermercado', done: false },
-        { id: 2, description: 'Trocar a lâmpada do freio do carro', done: true },
-        { id: 3, description: 'Começar a 3ª parte da série sobre React', done: false }
+        { id: 1, description: "Ir ao supermercado", done: false },
+        {
+          id: 2,
+          description: "Trocar a lâmpada do freio do carro",
+          done: true
+        },
+        {
+          id: 3,
+          description: "Começar a 3ª parte da série sobre React",
+          done: false
+        }
       ]
     }
   }
@@ -18,17 +27,17 @@ class App extends Component {
     this.setState(state);
   }
 
-  removeTask(taskId) {
+  removeTask = (taskId) => () => {
     let state = Object.assign({}, this.state);
     state.tasks = state.tasks.filter(t => t.id !== taskId);
     this.setState(state);
   }
 
-  completeTask(taskId) {
-    this.modifyTaskStatus(taskId, true);
+  completeTask = (taskId) => () => {
+    this.modifyTaskStatus(taskId, true)
   }
 
-  resetTask(taskId) {
+  resetTask = (taskId) => () => {
     this.modifyTaskStatus(taskId, false);
   }
 
@@ -36,13 +45,17 @@ class App extends Component {
     return (
       <div>
         <ul>
-          { this.state.tasks.map(task =>
-            <li key={ task.id }>
-              { task.description }
-              { !task.done ? <button onClick={ (e) => this.completeTask(task.id) }>Finalizar</button> : null }
-              { task.done ? <button onClick={ (e) => this.resetTask(task.id) }>Reativar</button> : null }
-              <button onClick={ (e) => this.removeTask(task.id) }>Remover</button>
-            </li>)}
+          {this.state.tasks.map(task => (
+            <li key={task.id}>
+              {task.description}
+              <TaskButtons
+                task={task}
+                completeTask={this.completeTask}
+                resetTask={this.resetTask}
+                removeTask={this.removeTask}
+              />
+            </li>
+          ))}
         </ul>
       </div>
     );
